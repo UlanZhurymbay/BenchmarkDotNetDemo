b42169f40b1b0b811f8936d365b04a657de0a042
UlanZhurymbay <zhurimbay@gmail.com>
2023-01-09 22:43:23 +0600

    проверка на производительностью приведение типов

---
+++ BenchmarkDotNetDemo/CastObject.cs
@@ -0,0 +1,54 @@
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDotNetDemo;
+[RankColumn ]
+[MemoryDiagnoser]
+public class CastObject
+{
+    private object _b = new B{ Name = "B"};
+    private const int Capacity = 2000;
+
+    [Benchmark(Description = "A class from is")]
+    public A First()
+    {
+        var a = new A();
+        for (var i = 0; i < Capacity; i++)
+        {
+            if (_b is A)
+                a = (A)_b;
+        }
+        return a;
+    }
+
+    [Benchmark(Description = "A class from as")]
+    public A Second()
+    {
+        var a = new A();
+        for (var i = 0; i < Capacity; i++)
+        {
+            var b = _b as A;
+            if (b != null)
+                a = b;
+        }
+        return a;
+    }
+    [Benchmark(Description = "A class pattern matching")]
+    public A Third()
+    {
+        var a = new A();
+        for (var i = 0; i < Capacity; i++)
+        {
+            if (_b is A b)
+                a = b;
+        }
+        return a;
+    }
+}
+
+public class A
+{
+    public string Name { get; set; } = "A";
+}
+public class B : A
+{
+}
--- BenchmarkDotNetDemo/Program.cs
+++ BenchmarkDotNetDemo/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//TODO "cd .\BenchmarkDotNetDemo\"
 //TODO command for run in the terminal -> "dotnet run -p BenchmarkDotNetDemo.csproj -c Release"
 
 using BenchmarkDotNet.Running;
 using BenchmarkDotNetDemo;
 
-BenchmarkRunner.Run<MemoryBenchMarkerDemo>();
+BenchmarkRunner.Run<CastObject>();
+//BenchmarkRunner.Run<MemoryBenchMarkerDemo>();
